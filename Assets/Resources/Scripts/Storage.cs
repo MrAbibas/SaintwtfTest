12ed747cdaca0ab8d076cca076fa53072849b321
MrAbibas <prowodniktoka@gmail.com>
2022-11-14 16:24:02 +0200

    fix and add change count events to Storage script

--- Assets/Resources/Scripts/Storage.cs
+++ Assets/Resources/Scripts/Storage.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 internal class Storage : MonoBehaviour
 {
@@ -11,18 +12,24 @@ internal class Storage : MonoBehaviour
     private Vector3 spacing;
     private Vector3[] _resourcePositions;
     private Stack<Resource> _stack;
+    internal UnityEvent onIncreaseCount;
+    internal UnityEvent onDecreaseCount;
 
     internal ResourceType ResourceType => resourceData.ResourceType;
     internal int ResourceCount => _stack.Count;
-    internal int maxResourceCount { get; private set; }
+    internal int MaxResourceCount { get; private set; }
 
-    private void Start()
+    private void Awake()
     {
         _stack = new Stack<Resource>();
         SetResourcePositions();
+        onIncreaseCount = new UnityEvent();
+        onDecreaseCount = new UnityEvent();
     }
     private void SetResourcePositions()
     {
+        Debug.Log(spacing);
+        Debug.Log(resourceData.Size);
         Vector3 resourceSize = resourceData.Size + spacing;
         Vector3Int size = new Vector3Int
         {
@@ -30,11 +37,11 @@ internal class Storage : MonoBehaviour
             y = (int)floor.localScale.y,
             z = (int)(floor.localScale.z / resourceSize.z)
         };
-        maxResourceCount = size.x * size.z;
-        _resourcePositions = new Vector3[maxResourceCount];
+        MaxResourceCount = size.x * size.z;
+        _resourcePositions = new Vector3[MaxResourceCount];
         Vector3 resourcePosOffset = floor.localScale / 2;
         resourcePosOffset.y = floor.position.y;
-        for (int i = 0; i < maxResourceCount; i++)
+        for (int i = 0; i < MaxResourceCount; i++)
             _resourcePositions[i] = new Vector3()
             {
                 x = resourceSize.x / 2 + i / size.z * resourceSize.x,
@@ -42,20 +49,23 @@ internal class Storage : MonoBehaviour
                 z = resourceSize.z / 2 + i % size.z * resourceSize.z,
             } - resourcePosOffset;
     }
-    internal void PushResource(Resource resource)
+    internal bool PushResource(Resource resource)
     {
-        if (resource.ResourceData.ResourceType != resourceData.ResourceType)
-            return;
+        if (resource.ResourceData.ResourceType != resourceData.ResourceType || _stack.Count >= MaxResourceCount)
+            return false;
 
         resource.transform.SetParent(transform, true);
         resource.transform.localPosition = _resourcePositions[_stack.Count];
         _stack.Push(resource);
+        onIncreaseCount?.Invoke();
+        return true;
     }
     internal Resource PopResource()
     {
-        if (_stack.Count > 0)
-            return _stack.Pop();
-        else
+        if (_stack.Count == 0)
             return null;
+
+        onDecreaseCount?.Invoke();
+        return _stack.Pop();
     }
 }
